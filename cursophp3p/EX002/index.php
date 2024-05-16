24c72ba47f1ef038d3067c11a8eff7e1b0452dbb
Antonio Luis <109863624+AntonioLLuis@users.noreply.github.com>
2024-05-16 18:30:56 -0300

    Add files via upload

---
+++ cursophp3p/EX002/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade</title>
+</head>
+<body>
+
+<?php 
+$cidade = "Ribeirão das Neves";
+$estado = "MG";
+$pais = "Brasil";
+
+echo "Eu moro em $cidade, $estado, $pais."
+?>
+
+    
+</body>
+</html>
---
+++ cursophp3p/aula000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nossa Primeira Página</title>
+</head>
+<body>
+    <h1>
+
+    <p>Esta é a minha primeira página</p>
+    </h1>
+
+    <?php 
+    print "Olá Mundo novo!"
+    ?>
+</body>
+</html>
